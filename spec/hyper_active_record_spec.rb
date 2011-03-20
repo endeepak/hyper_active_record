4a83013e761ec9473c3464e8e37db526ff0649aa
Deepak N <endeep123@gmail.com>
2011-03-20 12:58:44 +0530

    Refactored specs

--- spec/hyper_active_record_spec.rb
+++ spec/hyper_active_record_spec.rb
@@ -1,41 +1,47 @@
 require 'spec_helper'
 
 describe HyperActiveRecord::QueryMethods do
-  describe "all" do
-    before(:each) do
-      class Project < ActiveRecord::Base
-        scope :started_after, lambda { |time| where('start_date > ?', time) }
-        scope :completed, lambda { |time| where('end_date IS NOT NULL') }
-      end
+  before(:each) do
+    class Project < ActiveRecord::Base
+      scope :started_after, lambda { |time| where('start_date > ?', time) }
+      scope :completed, lambda { |time| where('end_date IS NOT NULL') }
     end
+  end
 
-    after(:each) do
-      Project.scopes.clear
-      Project.delete_all
-    end
+  after(:each) do
+    Project.scopes.clear
+    Project.delete_all
+  end
 
-    it "should be queryable with scope name" do
+  context "with scope name" do
+    it "should apply scope" do
       old_project = Factory(:project, :start_date => 2.years.ago)
       new_project = Factory(:project, :start_date => Date.today)
 
       Project.all(:conditions => {:started_after => 1.year.ago}).should == [new_project]
     end
+  end
 
-    it "should be queryable with combination of column name and scope name" do
+  context "with combination of column name and scope name" do
+    it "should apply scoped and match by column value" do
       old_critical_project = Factory(:project, :start_date => 2.years.ago, :priority => 1)
       new_simple_project = Factory(:project, :start_date => Date.today, :priority => 2)
       new_critical_project = Factory(:project, :start_date => Date.today, :priority => 1)
 
       Project.all(:conditions => {:started_after => 1.year.ago, :priority => 2}).should == [new_simple_project]
     end
+  end
 
-    it "should raise error when scope or column does not exist" do
+  context "when scope or column does not exist" do
+    it "should raise error" do
       lambda {
         Project.all(:conditions => {:non_existing_scope => 1.year.ago})
       }.should raise_error(/no such column: projects.non_existing_scope/)
     end
+  end
 
-    it "should apply scope when scope name is same as column name" do
+  context "context when scope name is same as column name" do
+    it "should apply scope" do
       Project.class_eval { scope :priority, lambda { |value| where('priority <= ?', value) } }
       project_1 = Factory(:project, :priority => 1)
       project_2 = Factory(:project, :priority => 2)
@@ -43,17 +49,21 @@ describe HyperActiveRecord::QueryMethods do
 
       Project.all(:conditions => {:priority => 2}).should =~ [project_1, project_2]
     end
+  end
 
-    context "for scope which does not take parameters" do
-      it "should apply scope when value is true" do
+  context "for scope which does not take parameters" do
+    context " when value is true" do
+      it "should apply scope" do
         project_1 = Factory(:project, :end_date => Date.today)
         project_2 = Factory(:project, :end_date => nil)
         project_3 = Factory(:project, :end_date => Date.today)
 
         Project.all(:conditions => {:completed => true}).should =~ [project_1, project_3]
       end
+    end
 
-      it "should not apply scope when value is false" do
+    context "when value is false" do
+      it "should not apply scope" do
         project_1 = Factory(:project, :end_date => Date.today)
         project_2 = Factory(:project, :end_date => nil)
         project_3 = Factory(:project, :end_date => Date.today)
