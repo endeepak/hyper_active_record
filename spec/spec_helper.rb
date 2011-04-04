812f3e68df12983c0c6720a9690aff11d4745610
Deepak N <endeep123@gmail.com>
2011-04-04 14:36:06 +0530

    Removed commented code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,3 @@ require File.dirname(__FILE__)+ '/factories'
 
 require 'hyper_active_record'
 require File.expand_path(File.dirname(__FILE__) + "/db/init.rb")
-
-RSpec.configure do |config|
-  # config.before(:each) { Project.delete_all }
-  # config.mock_with :mocha
-end
